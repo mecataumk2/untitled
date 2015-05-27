ca3649d4cb2a249f5f80de24b12bd55963cfda12
sdlee <mecataumk2@gmail.com>
2015-05-27 19:36:16 +0900

    Extract e-mail

--- 02.py
+++ 02.py
@@ -1,14 +1,12 @@
 file_test = open('sample.txt', 'rt') 
 
- 
 print type(file_test)
 print file_test
 
 lines = file_test.readlines()
 
 print type(lines)
-print lines[1: 3]
-print len(lines[1:2])
+
 
 range_1 = range(100, 105)
 print type(range_1)
@@ -19,10 +17,20 @@ for number in range_1:
     print number
 
  
-for line in lines:
-    print line
+for line in lines[1:3]:
+   # print line
+   # a=line.split(",")
+   split = line.split(",")
+   email = split[3]
+   print email
+  #  print a[3]
 
-#print type(line)
+
+#a=line.split(",")
+#print a
+
+
+
 
 #if isinstance(line, str) and '@' in str
 #    print line
